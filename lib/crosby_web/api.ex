d598c8479b7831d98f199bded2d43fd72b592340
mehbark <terezi@pyrope.net>
2025-09-17 20:39:01 -0400

    add `/api/categories.zip` handler

--- lib/crosby_web/api.ex
+++ lib/crosby_web/api.ex
@@ -4,6 +4,12 @@ defmodule CrosbyWeb.Api do
   use CrosbyWeb, :controller
   alias Crosby.{Repo, Category, Entry}
 
+  @special_categories MapSet.new(~w[psa ids lnr soo pro backup])
+
+  def special_category?(name) do
+    MapSet.member?(@special_categories, name)
+  end
+
   def entries_for_category(name) do
     query =
       from entry in Entry,
@@ -20,10 +26,28 @@ defmodule CrosbyWeb.Api do
     "#EXTM3U\n" <> tracks
   end
 
-  def category(conn, params) do
+  def category(conn, %{"category" => category}) do
     conn
     |> put_resp_header("content-type", "application/vnd.apple.mpegurl")
-    |> text(m3u(params |> Map.get("category")))
+    |> text(m3u(category))
+  end
+
+  def categories do
+    Repo.all(from category in Category, select: category.name)
+  end
+
+  def playlists(conn, _params) do
+    categories = categories() |> Enum.reject(&special_category?/1)
+
+    files =
+      categories
+      |> Task.async_stream(fn cat -> {String.to_charlist(cat <> ".m3u"), m3u(cat)} end)
+      |> Enum.map(fn {:ok, file} -> file end)
+      |> Enum.to_list()
+
+    {:ok, {_filename, zip}} = :zip.create(~c"playlists.zip", files, [:memory])
+
+    conn |> send_download({:binary, zip}, filename: "playlists.zip")
   end
 
   # TODO: distinguish "special" categories (like psas),
